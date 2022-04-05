4ed19e009922a343b6dc9ffe98d8ae03601beae6
Amie Shanaya Wijeratne <72076192+22012825@users.noreply.github.com>
2022-04-06 00:33:46 +0100

    Added Polymorphic Display Methods

--- ConsoleAppProject/App04/Post.cs
+++ ConsoleAppProject/App04/Post.cs
@@ -59,21 +59,55 @@ namespace ConsoleAppProject.App04
 		{
 			comments.Add(text);
 		}
+	
+	///<summary>
+	/// Display the details of this post.
+	/// 
+	/// (Currently: Print to the text terminal. This is simulating display 
+	/// in a web browser for now.)
+	///</summary>
+	public void Display()
+	{
+		Console.WriteLine();
+		Console.WriteLine($"    Author: {Username}");
+		Console.WriteLine($"    Time Elpased: {FormatElapsedTime(Timestamp)}");
+		Console.WriteLine();
 
-    ///<summary>
-    /// Create a string describing a time point in the past in terms 
-    /// relative to current time, such as "30 seconds ago" or "7 minutes ago".
-    /// Currently, only seconds and minutes are used for the string.
+		if (likes > 0)
+		{
+			Console.WriteLine($"    Likes:  {likes}  people like this.");
+		}
+		else
+		{
+			Console.WriteLine();
+		}
 
-    /// </summary>
-    /// <param name="time">
-    ///  The time value to convert (in system milliseconds)
-    /// </param> 
-    /// <returns>
-    /// A relative time string for the given time
+		if (comments.Count == 0)
+		{
+			Console.WriteLine("    No comments.");
+		}
+		else
+		{
+			Console.WriteLine($"    {comments.Count}  comment(s). Click here to view.");
+		}
+	}
 
-    /// </returns>      
-    private String FormatElapsedTime(DateTime time)
+}
+}
+	///<summary>
+	/// Create a string describing a time point in the past in terms 
+	/// relative to current time, such as "30 seconds ago" or "7 minutes ago".
+	/// Currently, only seconds and minutes are used for the string.
+
+	/// </summary>
+	/// <param name="time">
+	///  The time value to convert (in system milliseconds)
+	/// </param> 
+	/// <returns>
+	/// A relative time string for the given time
+
+	/// </returns>      
+	private String FormatElapsedTime(DateTime time)
 	{
 		DateTime current = DateTime.Now;
 		TimeSpan timePast = current - time;
