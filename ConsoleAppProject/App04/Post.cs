4af92e95279be0623a691d9c590194db21866008
Amie Shanaya Wijeratne <72076192+22012825@users.noreply.github.com>
2022-04-06 00:19:56 +0100

    Added Inheritance to PhotoPost

---
+++ ConsoleAppProject/App04/Post.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic
+using System.Text;	
+
+namespace ConsoleAppProject.App04
+	
+	public class Post ()
+	{
+		private int likes;
+
+		private readonly List<String> comments;
+
+		// username of the post's author
+		public String Username { get; }
+
+		public DateTime Timestamp { get; }
+
+		/// <summary>
+        /// 
+        /// </summary>
+		
+		public Post(string author)
+		
+		{
+		this.Username = author;
+		Timestamp = DateTime.Now;
+		
+		likes = 0;
+		comments = new List<String>();
+
+
+		/// <summary>
+		/// Record one more 'Like' indication from a user.
+		/// </summary>
+		public void Like()
+		{
+			likes++;
+		}
+
+		///<summary>
+		/// Record that a user has withdrawn his/her 'Like' vote.
+		///</summary>
+		public void Unlike()
+	{
+		if (likes > 0)
+		{
+			likes--;
+		}
+	}
+
+	
+		///<summary>
+		/// Add a comment to this post.
+		/// </summary>
+		/// <param name="text">
+		/// The new comment to add.
+		/// </param>        
+		public void AddComment(String text)
+		{
+			comments.Add(text);
+		}
+
+    ///<summary>
+    /// Create a string describing a time point in the past in terms 
+    /// relative to current time, such as "30 seconds ago" or "7 minutes ago".
+    /// Currently, only seconds and minutes are used for the string.
+
+    /// </summary>
+    /// <param name="time">
+    ///  The time value to convert (in system milliseconds)
+    /// </param> 
+    /// <returns>
+    /// A relative time string for the given time
+
+    /// </returns>      
+    private String FormatElapsedTime(DateTime time)
+	{
+		DateTime current = DateTime.Now;
+		TimeSpan timePast = current - time;
+
+		long seconds = (long)timePast.TotalSeconds;
+		long minutes = seconds / 60;
+
+		if (minutes > 0)
+		{
+			return minutes + " minutes ago";
+		}
+		else
+		{
+			return seconds + " seconds ago";
+		}
+	}	
+}
